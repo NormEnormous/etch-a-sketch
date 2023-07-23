7da2823e90431ad0854ef3fc779917b065974faf
NormEnormous <130330451+NormEnormous@users.noreply.github.com>
2023-07-23 15:48:40 -0400

    Initial Commit, Pseudocode and HTML skeleton

---
+++ javascript.js
@@ -0,0 +1,22 @@
+// 1. Create 16x16 grid of square divs
+//   a) create the divs using javascript
+//   b) add the divs to container div
+//   c) make the divs appear square, and in a grid with flexbox
+
+// 2. Set up hover effect, causing grid divs to change color when hovered
+//   a) create the hover effect on mousedown with javascript
+//   b) add a new class, or change the divs background color
+
+// 3. Add a button to select grid size
+//   a) button should prompt for size of grid
+//   b) limit size of grid to 100x100
+//   c) grid should fill the same total space, regardless of grid size
+
+// 4. Add five buttons.
+//   a) button one will select color for color mode
+//   b) button tw will select color mode
+//   c) button three will select ghost mode
+//     i) ghost mode will progressively increase opacity of grids by 10%
+//   d) button four will select rainbow mode
+//     i) rainbow mode will change color of divs randomly
+//   e) button five will clear our workspace.
