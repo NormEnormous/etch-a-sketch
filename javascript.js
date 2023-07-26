7641a5202b729e3061ad4ecbfdbbaffd8e0a4f4f
NormEnormous <130330451+NormEnormous@users.noreply.github.com>
2023-07-25 20:50:05 -0400

    Functioning add and remove grid

--- javascript.js
+++ javascript.js
@@ -3,22 +3,38 @@
 //   b) add the divs to container div
 //   c) make the divs appear square, and in a grid with flexbox
 
-let gridSize = 16;
-
-
+let gridSize = 36;
+let basis = 100 / Math.sqrt(gridSize);
 
 const grid = document.querySelector('.gridContainer');
-function selectSize() {
+const currentGrids = document.getElementsByClassName('grid');
+
+function removeGrids() {
+    for (let i = currentGrids.length; i > 0; i--) {
+        const div = document.querySelector(".grid")
+        grid.removeChild(div);
+        console.log(currentGrids.length);
+    }
+}
+function addGrids() {
     for (let i = 0; i < gridSize; i++) {
         const div = document.createElement('div');
         div.classList.add('grid');
         grid.appendChild(div);
+        console.log(currentGrids.length);
     }
 }
-selectSize();
+function changeGrids() {
+    removeGrids();
+    addGrids();
+    colorSquares();
+}
 
+
+
+addGrids();
 // 2. Click and hover events. performance issues.
-
+function colorSquares() {
 const gridSquares = document.querySelectorAll('.grid');
 for(let i = 0; i < gridSquares.length; i++) {
     gridSquares[i].addEventListener('mouseover', function(e) {
@@ -32,14 +48,26 @@ for(let i = 0; i < gridSquares.length; i++) {
         gridSquares[i].classList.toggle('colored');
     })
 }
-
+}
+colorSquares();
 // 3. Add a button to select grid size
 //   a) button should prompt for size of grid
 //   b) limit size of grid to 100x100
 //   c) grid should fill the same total space, regardless of grid size
+const gridButton = document.querySelector('#gridButton');
+gridButton.addEventListener('click', () => changeGridSize());
 
-
-
+function changeGridSize() {
+    gridSize = prompt('Enter a square number between 1 and 100');
+    if (gridSize === 16 || gridSize == 25 || gridSize == 36 || gridSize == 49 || gridSize == 64 || gridSize == 81 || gridSize == 100) {
+        changeGrids();
+    } else if (gridSize === null || gridSize === '') {
+        return;
+    } else {
+        alert('That was not a square number between 1 and 100. Please try again.');
+        changeGrids();
+    }
+}
 
 
 
